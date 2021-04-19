7d1cfa9e8fdbde85239783b805a9bfabc7cc1c03
rootCurse <mix.kras09@ya.ru>
2021-04-19 21:26:42 +0300

    homework3

---
+++ homework3/homework3.cpp
@@ -0,0 +1,318 @@
+﻿#include "pugixml.hpp"
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+
+
+using namespace std;
+
+multimap <string, int> stopTram;
+set<string> tramR;
+multimap <string, int> stopBus;
+set<string> busR;
+multimap <string, int> stopTrolleybus;
+set<string> trolleybusR;
+string zeroName;
+vector<pair<string, int>> streets;
+
+struct xmlTab 
+{
+	string typeOfTransport;
+	bool stop = false;
+	string site;
+	string firstSite;
+	string secondSite;
+	double coordinatesOne = 0;
+	double coordinatesTwo = 0;
+};
+
+vector<xmlTab> container;
+void mySplit(string str, string& subStr1, string& subStr2)
+{
+	auto sp = str.find(',');
+	for (auto i = 0; i < str.length(); i++) 
+	{
+		if (i < sp)
+			subStr1.push_back(str[i]);
+		if (i > sp)
+			subStr2.push_back(str[i]);
+	}
+}
+
+void Parsing(string str, int i, int j)
+{
+	if (i % 8 == 1)
+		container[j].typeOfTransport = str;
+	if (i % 8 == 2)
+	{
+		if (j == 0)
+			zeroName = str;
+		else
+			container[j].stop = (str == zeroName);
+	}
+	if (i % 8 == 5)
+	{
+		int flag;
+		container[j].site = str;
+		string s1, s2;
+		mySplit(str, s1, s2);
+		container[j].firstSite = s1;
+		container[j].secondSite = s2;
+		flag = 0;
+		if (container[j].stop == 1)
+		{
+			if (!streets.empty())
+				for (auto i = 0; i < streets.size(); i++)
+					if (streets[i].first == container[j].firstSite && (!container[j].firstSite.empty()))
+					{
+						flag = 1;
+						streets[i].second += 1;
+					}
+					else if (streets[i].first == container[j].secondSite && (!container[j].firstSite.empty()))
+					{
+						flag = 2;
+						streets[i].second += 1;
+					}
+			    if (flag == 0)
+					streets.push_back(make_pair(container[j].firstSite, 1));
+				else
+				{
+					streets.push_back(make_pair(container[j].firstSite, 1));
+					streets.push_back(make_pair(container[j].secondSite, 1));
+				}
+		}
+	}
+	if (i % 8 == 6)
+	{
+		string buffer;
+		buffer.clear();
+		if (container[j].stop == 1)
+		{
+			if (j < 888)
+			{
+				for (auto value : str)
+				{
+					if (value != ',')
+						buffer.push_back(value);
+					else
+					{
+						stopTram.insert(make_pair(buffer, j));
+						tramR.insert(buffer);
+						buffer.clear();
+					}
+				}
+				stopTram.insert(make_pair(buffer, j));
+				tramR.insert(buffer);
+				buffer.clear();
+			}
+			else if (j >= 888 && j < 2080)
+			{
+				for (auto value : str)
+					if (value != ',')
+						buffer.push_back(value);
+					else
+					{
+						stopTrolleybus.insert(make_pair(buffer, j));
+						trolleybusR.insert(buffer);
+						buffer.clear();
+					}
+				stopTrolleybus.insert(make_pair(buffer, j));
+				trolleybusR.insert(buffer);
+				buffer.clear();
+			}
+			else
+			{
+				for (auto value : str)
+					if (value != ',')
+						buffer.push_back(value);
+					else
+					{
+						stopBus.insert(make_pair(buffer, j));
+						busR.insert(buffer);
+						buffer.clear();
+					}
+				stopBus.insert(make_pair(buffer, j));
+				busR.insert(buffer);
+				buffer.clear();
+			}
+		}
+	}
+	if (i % 8 == 7)
+	{
+		double coordinates1, coordinates2;
+		string subStr1, subStr2;
+		mySplit(str, subStr1, subStr2);
+		coordinates1 = stof(subStr1);
+		coordinates2 = stof(subStr2);
+		container[j].coordinatesOne = coordinates1;
+		container[j].coordinatesTwo = coordinates2;
+	}
+}
+
+double lenght(double firstStop, double secondStop)
+{
+	double firstSin;
+	firstSin = sin((container[secondStop].coordinatesOne - container[firstStop].coordinatesOne) / 2);
+	double secondSin;
+	secondSin = sin((container[secondStop].coordinatesTwo - container[firstStop].coordinatesTwo) / 2);
+	return asin(sqrt(firstSin * firstSin + secondSin * secondSin * cos(container[firstStop].coordinatesOne) * cos(container[secondStop].coordinatesOne)));
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int index, indexOfStop = 0;
+	container.resize(8000);
+	
+	//******************************************************UTF**********************************************************************
+	system("chcp 65001");
+	
+	
+	//*******************************************************OPENXML****************************************************************
+	pugi::xml_document input;
+	if(!input.load_file(R"(C:\Users\root\source\repos\homework3\homework3\input.xml)")) return -1;
+	
+	
+	
+	//*******************************************************INPUT*******************************************************************
+	for (pugi::xml_node tools = input.child("dataset").child("transport_station"); tools; tools = tools.next_sibling()) 
+	{
+		index = 0;
+		for (pugi::xml_node tool = tools.first_child(); tool; tool = tool.next_sibling())
+		{
+			string s = tool.child_value();
+			Parsing(s, index, indexOfStop);
+			index++;
+		}
+		indexOfStop++;
+	}
+
+
+
+	//********************************************************TASK1**************************************************************************
+	string route;
+	int countOfStop = 0;
+	cout << "Task 1 :" << endl;
+	for (auto value : busR)
+		if (countOfStop < stopBus.count(value))
+		{
+			countOfStop = stopBus.count(value);
+			route = value;
+		}
+	cout << "Number of route bus: " << route << " - Quantity of bus stop: " << countOfStop << endl;
+	countOfStop = 0;
+	for (auto value : trolleybusR)
+		if (countOfStop < stopTrolleybus.count(value))
+		{
+			countOfStop = stopTrolleybus.count(value);
+			route = value;
+		}
+	cout << "Number of route trolleybus: " << route << " - Quantity of trolleybus stop: " << countOfStop << endl;
+	countOfStop = 0;
+	for (auto value : tramR)
+		if (countOfStop < stopTram.count(value))
+		{
+			countOfStop = stopTram.count(value);
+			route = value;
+		}
+	cout << "Number of route tram: " << route << " - Quantity of tram stop: " << countOfStop << endl;
+	
+	
+
+	//*********************************************************TASK2***************************************************************************
+	cout << "Task 2: " << endl;
+	double thisLenght = 0, maxLenght = 0;
+	string rout;
+	double tempValue;
+	for (auto value : busR)
+	{
+		multimap<string, int>::iterator iter = stopBus.find(value);
+		if (iter != stopBus.end())
+			while (iter->first == value)
+			{
+				tempValue = iter->second;
+				iter++;
+				if (iter != stopBus.end())
+					if (iter->first == value)
+						thisLenght += lenght(tempValue, iter->second);
+					else
+						continue;
+				else
+					break;
+			}
+		if (maxLenght < thisLenght)
+		{
+			maxLenght = thisLenght;
+			rout = value;
+		}
+		thisLenght = 0;
+	}
+	cout << "Rout with max lenght for bus - " << rout << endl;
+	maxLenght = 0;
+	for (auto value : tramR)
+	{
+		multimap<string, int>::iterator iter = stopTram.find(value);
+		if (iter != stopTram.end())
+			while (iter->first == value)
+			{
+				tempValue = iter->second;
+				iter++;
+				if (iter != stopTram.end())
+					if (iter->first == value)
+						thisLenght += lenght(tempValue, iter->second);
+					else
+						continue;
+				else
+					break;
+			}
+		if (maxLenght < thisLenght)
+		{
+			maxLenght = thisLenght;
+			rout = value;
+		}
+		thisLenght = 0;
+	}
+	cout << "Rout with max lenght for tram - " << rout << endl;
+	maxLenght = 0;
+	for (auto value : trolleybusR)
+	{
+		multimap<string, int>::iterator iter = stopTrolleybus.find(value);
+		if (iter != stopTrolleybus.end())
+			while (iter->first == value)
+			{
+				tempValue = iter->second;
+				iter++;
+				if (iter != stopTrolleybus.end())
+					if (iter->first == value)
+						thisLenght += lenght(tempValue, iter->second);
+					else
+						continue;
+				else
+					break;
+			}
+		if (maxLenght < thisLenght)
+		{
+			maxLenght = thisLenght;
+			rout = value;
+		}
+		thisLenght = 0;
+	}
+	cout << "Rout with max lenght for trolleybus - " << rout << endl;
+
+
+	//*********************************************************TASK3***************************************************************************
+	int streetStop = 0;
+	int position = 0;
+	for (auto it = 0; it < streets.size(); it++)
+		if (streets[it].second > streetStop && !streets[it].first.empty())
+		{
+			streetStop = streets[it].second;
+			position = it;
+		}
+	cout << "Task 3 :\nName of street with max quantity of stop - " << streets[position].first << endl;
+}
